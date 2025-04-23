2a36460c7f59cece5807d58ec88d951b8cc6849f
nikhilkanthi-creator <nkanthi@chicagobooth.edu>
2025-04-23 01:59:08 +0000

    almost done

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -1,5 +1,17 @@
 class DiceController < ApplicationController
+
   def home
     render({ :template => "dice_templates/home"})
+  end
+
+  def dynamicpath
+    @dice = params.fetch("dice").to_i
+    @sides = params.fetch("sides").to_i
+    @rolls = []
+    @dice.times do
+      dice = rand(1..@sides) # generate a random number
+      @rolls.push(dice) # fill up the array
+    end
+    render({ :template => "dice_templates/dynamic"})
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-
   get("/", { :controller => "dice", :action => "home"})
+
+  get("dice/:dice/:sides", { :controller => "dice", :action => "dynamicpath"})
+
 end
